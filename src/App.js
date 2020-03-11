a60a71177576b6623669c3d0775ffc2f76ef2c0f
EmiKusaba <eeemi615@gmail.com>
2020-03-10 20:57:19 -0500

    added function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      todos: [],
+      isClicked: false
+    }};
+    onChange = e => {
+      this.setState({
+        text: e.target.value
+      })
+    }
+   
+    render() {
+      console.log("This is state***", this.state.isClicked)
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>To Do List</h1>
+       <form>
+       <input></input>
+       </form>
+        <button onClick = {this.props.isClicked}>Submit</button>
       </header>
     </div>
   );
 }
-
+}  
 export default App;
